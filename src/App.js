9423e004380ed525edbdcef3bbd9f6041895f6c3
Violetta Doroshko <violet.doroshko@gmail.com>
2021-04-29 12:21:45 +0300

    change ide to webstorm

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import React from "react";
 import './App.css';
-import {Button, Container} from "react-bootstrap";
+import SearchBar from "./component/SearchBar";
 
 function App() {
     return (
-        <Container className="Log in">
-            <body>
-            <h2>Login Form</h2>
-            <h3>Personal Info</h3>
-
-            <form>
-                <div className="container">
-                    <label htmlFor="uname"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="uname" required>
-                    </input>
-
-                    <label htmlFor="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" required>
-                    </input>
-
-                    <label htmlFor="psw"><b>Repeat Password</b></label>
-                    <input type="password" placeholder="Repeat Password" name="psw" required>
-                    </input>
-
-                    <Button type="submit">Register</Button>
-
-                </div>
-            </form>
-            </body>
-        </Container>);
+        SearchBar()
+    )
 }
 
 export default App;
