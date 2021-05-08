57e6ae65671166b82aaca2fe5068c6ae788798b7
Uladzislau Daroshka <uladzislau.daroshka@gmail.com>
2021-05-08 14:31:59 +0300

    refactor: all pages

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import Aviasales from './containers/Aviasales';
 import LogSignInfo from './components/LogSignInfo';
+import LogPersonalInfo from './components/LogPersonalInfo';
+import Sign from './components/Sign';
+import TicketCard from './components/TicketCard';
 
 //todo: fix /#/
 const Main = () => (
   <HashRouter basename="/">
     <Switch>
       <Route path="/" exact component={Aviasales} />
-      <Route path="/create" exact component={LogSignInfo} />
+      <Route path="/1" exact component={LogSignInfo} />
+      <Route path="/2" exact component={LogPersonalInfo} />
+      <Route path="/3" exact component={Sign} />
+      <Route path="/4" exact component={TicketCard} />
     </Switch>
   </HashRouter>
 );
